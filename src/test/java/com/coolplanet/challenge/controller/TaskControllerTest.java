f316c69a3e3067a79cf9d5fe4e6c3cf585fbfc13
612750911 <jack.3.hughes@bt.com>
2022-08-19 09:36:10 +0100

    testing of controller layer

--- src/test/java/com/coolplanet/challenge/controller/TaskControllerTest.java
+++ src/test/java/com/coolplanet/challenge/controller/TaskControllerTest.java
@@ -22,6 +22,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.coolplanet.challenge.entity.RecordedTaskDTO;
 import com.coolplanet.challenge.entity.TaskDTO;
+import com.coolplanet.challenge.exception.ErrorResponse;
+import com.coolplanet.challenge.exception.ResourceNotFoundException;
 import com.coolplanet.challenge.service.TaskService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -39,11 +41,15 @@ public class TaskControllerTest {
 	private ObjectMapper mapper = new ObjectMapper();
 	
 	private final String postUri = "/v1/coolplanet/task";
+	private final String getUri = "/v1/coolplanet/task";
 	
 	/*
 	 * POST tests
 	 */
-	
+	/**
+	 * Success Scenario Testing
+	 * @throws Exception
+	 */
 	@Test
 	void addTask_Success() throws Exception {
 		// Initialise Vars
@@ -67,10 +73,79 @@ public class TaskControllerTest {
 				);
 		
 	}
+
 	/*
 	 * GET tests
 	 */
 	
+	/**
+	 * Successful Scenario testing
+	 * @throws Exception
+	 */
+	@Test
+	void getAverageDuration_Success() throws Exception {
+		// Initialise Vars
+		Long duration = 500L;
+		Long id = 1L;
+		TaskDTO response = TaskDTO.builder().averageTaskDuration(duration).taskIdentifier(id).build();
+		
+		// set up mocks
+		Mockito.when(service.getAverageTaskDuration(Mockito.anyLong())).thenReturn(response);
+		
+		// build Request 
+		RequestBuilder request = buildGetRequest(id, getUri);
+		
+		// carry out request
+		MvcResult result = mockMvc.perform(request).andReturn();
+		
+		// Assertions
+		verify(service).getAverageTaskDuration(Mockito.anyLong());
+		
+		TaskDTO taskDto = mapper.readValue(result.getResponse().getContentAsString(), TaskDTO.class);
+		Assertions.assertAll(
+				() -> assertNotNull(result),
+				() -> assertNotNull(taskDto),
+				() -> assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus()),
+				() -> assertEquals(id, taskDto.getTaskIdentifier()),
+				() -> assertEquals(duration, taskDto.getAverageTaskDuration())
+				);
+	}
+
+	/**
+	 * Failure scenario testing for expected Exception which is handled by ExceptionHandler
+	 * @throws Exception
+	 */
+	@Test
+	void getAverageDuration_Exception() throws Exception {
+		// Initialise Vars
+		String errorMessage = "errorMessage";
+		Long id = 1L;
+		ResourceNotFoundException exception = new ResourceNotFoundException(errorMessage);
+		
+		// set up mocks
+		Mockito.when(service.getAverageTaskDuration(Mockito.anyLong())).thenThrow(exception);
+		
+		// build Request 
+		RequestBuilder request = buildGetRequest(id, getUri);
+		
+		// carry out request
+		MvcResult result = mockMvc.perform(request).andReturn();
+		
+		// Assertions
+		verify(service).getAverageTaskDuration(Mockito.anyLong());
+		
+		ErrorResponse errorResponse = mapper.readValue(result.getResponse().getContentAsString(), ErrorResponse.class);		
+		Assertions.assertAll(
+				() -> assertNotNull(result),				
+				() -> assertEquals(HttpStatus.NOT_FOUND.value(), result.getResponse().getStatus()),
+				() -> assertNotNull(errorResponse.getTimeStamp()),
+				() -> assertEquals(errorMessage, errorResponse.getMessage())				
+				);		
+	}
+	
+	/*
+	 * Helper Methods
+	 */
 	public <T> String createJson(T request) {		
 		String requestJson = "";
 		
@@ -90,5 +165,18 @@ public class TaskControllerTest {
 	            .content(createJson(request))
 	            .characterEncoding("utf-8");
 		return requestBuilder;
+	}
+	
+	public RequestBuilder buildGetRequest(Long id, String uri) {
+		StringBuilder sb = new StringBuilder(uri);
+		sb.append("/");
+		sb.append(id);
+		String updatedUri = sb.toString();
+		
+		RequestBuilder rb = MockMvcRequestBuilders.get(updatedUri)
+				.accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON)
+				.characterEncoding("utf-8");
+		return rb;
 	}
 }
