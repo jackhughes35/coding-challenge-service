b4e89bae80242f1000885701538a2e3706316067
612750911 <jack.3.hughes@bt.com>
2022-08-19 16:27:02 +0100

    added Query to get Average Task Duration

--- src/main/java/com/coolplanet/challenge/repository/RecordedTaskRepository.java
+++ src/main/java/com/coolplanet/challenge/repository/RecordedTaskRepository.java
@@ -1,6 +1,8 @@
 package com.coolplanet.challenge.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.coolplanet.challenge.entity.RecordedTask;
@@ -8,6 +10,6 @@ import com.coolplanet.challenge.entity.RecordedTask;
 @Repository
 public interface RecordedTaskRepository extends JpaRepository<RecordedTask, Long>{
 
-//	@Query("SELECT avg(duration) FROM RecordedTasks WHERE taskID = ")
-//	Long averageDurationByTaskIdentifier(Long id);
+	@Query(value = "SELECT avg(task_duration) FROM task_records where task_identifier = :id", nativeQuery = true)
+	public Long averageDurationByTaskIdentifier(@Param("id") Long id);
 }
--- src/main/java/com/coolplanet/challenge/repository/TaskRepository.java
+++ src/main/java/com/coolplanet/challenge/repository/TaskRepository.java
@@ -1,6 +1,8 @@
 package com.coolplanet.challenge.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.coolplanet.challenge.entity.Task;
@@ -8,4 +10,6 @@ import com.coolplanet.challenge.entity.Task;
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long>{
 
+	@Query(value = "SELECT avg(task_duration) FROM task_records where task_identifier = :id", nativeQuery = true)
+	public Long averageDurationByTaskIdentifier(@Param("average") Long average, @Param("id") Long id);
 }
