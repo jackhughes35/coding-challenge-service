2bbef42eb55f72f3875007b31707d1746b9595c3
612750911 <jack.3.hughes@bt.com>
2022-08-19 09:38:35 +0100

    updates to scan base packages for beans

--- src/main/java/com/coolplanet/challenge/CodingChallengeServiceApplication.java
+++ src/main/java/com/coolplanet/challenge/CodingChallengeServiceApplication.java
@@ -2,8 +2,10 @@ package com.coolplanet.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.coolplanet.challenge.*"})
 public class CodingChallengeServiceApplication {
 
 	public static void main(String[] args) {
