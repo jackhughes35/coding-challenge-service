d606267ddeaac8c32729cdf228983f3e169bbc44
612750911 <jack.3.hughes@bt.com>
2022-08-19 09:35:05 +0100

    added exception handling

---
+++ src/main/java/com/coolplanet/challenge/exception/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.coolplanet.challenge.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorResponse {
+	
+	private String message;
+	private String timeStamp;
+}
---
+++ src/main/java/com/coolplanet/challenge/exception/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.coolplanet.challenge.exception;
+
+public class ResourceNotFoundException extends Exception {
+
+	/**
+	 * default Serial ID
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+	
+}
