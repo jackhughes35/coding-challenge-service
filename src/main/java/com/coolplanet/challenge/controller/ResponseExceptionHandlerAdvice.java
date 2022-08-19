ec51c53c4261b7b9d69b4aed7e25ee6f3dd6d401
612750911 <jack.3.hughes@bt.com>
2022-08-19 09:52:33 +0100

    updated to include logging facility in exceptions

--- src/main/java/com/coolplanet/challenge/controller/ResponseExceptionHandlerAdvice.java
+++ src/main/java/com/coolplanet/challenge/controller/ResponseExceptionHandlerAdvice.java
@@ -7,22 +7,29 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.coolplanet.challenge.exception.ErrorResponse;
 import com.coolplanet.challenge.exception.ResourceNotFoundException;
 
+
 @RestControllerAdvice
 public class ResponseExceptionHandlerAdvice extends ResponseEntityExceptionHandler {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+	
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public final ResponseEntity<ErrorResponse> handleResourceNotFound(ResourceNotFoundException ex){
 		ErrorResponse errorResponse = ErrorResponse.builder().message(ex.getMessage()).timeStamp(LocalDateTime.now().toString()).build();
+		logger.error("Resource Not Found Exception :: {}", errorResponse.toString());
 		return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);		
 	}
 	
 	@ExceptionHandler(Exception.class)
 	public final ResponseEntity<ErrorResponse> handleInternalError(Exception ex){
 		ErrorResponse errorResponse = ErrorResponse.builder().message(ex.getMessage()).timeStamp(LocalDateTime.now().toString()).build();
+		logger.error("Exception {} caught :: {}", ex.getClass(), errorResponse.toString());
 		return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);		
 	}
 }
